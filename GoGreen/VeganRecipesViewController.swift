833a7e74bf9309ac4421ff02fd4b36d0087ab5f4
Katie <katie.hdz.2007@gmail.com>
2022-07-19 23:40:31 -0500

    Added the view controllers, as well as the buttons. I furthermore added the transition between the buttons and the next view controller.

---
+++ GoGreen/VeganRecipesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VeganRecipesViewController.swift
+//  GoGreen
+//
+//  Created by scholar on 7/19/22.
+//
+
+import UIKit
+
+class VeganRecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
